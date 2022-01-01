17054573ea23bdb66380fcf0bd8ed05703d9f380
ygtxr1997 <gavingreenson@icloud.com>
2022-01-01 11:53:36 +0800

    add: occlusion augmentation of ellipse and polygon

--- datasets/augment/rand_occ.py
+++ datasets/augment/rand_occ.py
@@ -21,7 +21,7 @@ Init Params:
     - fill: 'black' means black square;
             'gauss' means Gaussian noise square.
 """
-class RandomBlockOcc(object):
+class RandomBlock(object):
     fill_list = ['black', 'gauss',]
     def __init__(self,
                  lo: int,
@@ -30,7 +30,7 @@ class RandomBlockOcc(object):
         self.lo = lo
         self.hi = hi
         self.fill = fill
-        assert fill in RandomBlockOcc.fill_list
+        assert fill in RandomBlock.fill_list
 
     def __call__(self, img):
         ratio = np.random.randint(self.lo, self.hi) * 0.01
@@ -88,7 +88,7 @@ Init Params:
     - lo: lowest ratio (%) in range [lo, hi)
     - hi: highest ratio (%) in range [lo, hi)
 """
-class RandomRectOcc(object):
+class RandomRect(object):
     def __init__(self,
                  lo: int = 0,
                  hi: int = 36,):
@@ -133,10 +133,194 @@ class RandomRectOcc(object):
 
         return img_occ, msk
 
+
+""" Random Ellipse Occlusion
+This transform can be used for training and testing.
+Init Params:
+    - connected_num: the num of simply connected graphs (default: 1)
+    - lo_ratio: the min area ratio of occlusion (default: 0.2)
+    - hi_ratio: the max area ratio of occlusion (default: 0.4)
+    - use_rand_color: use random color or not (default: True)
+"""
+class RandomEllipse(object):
+    def __init__(self,
+                 connected_num:int = 1,
+                 lo_ratio: float = 0.2,
+                 hi_ratio: float = 0.4,
+                 use_rand_color:bool = True
+                 ):
+        self.connected_num = connected_num
+        self.lo_ratio = lo_ratio
+        self.hi_ratio = hi_ratio
+        self.use_rand_color = use_rand_color
+
+    def __call__(self, img):
+        face_arr = np.array(img)
+        height, width = img.size[1], img.size[0]
+        channel = 1 if len(face_arr.shape) == 2 else 3
+
+        # 1. Generate the ellipse (np.array), where 0 denotes no occlusion.
+        # The shape of ellipse (np.array) is (height, width) without channel dimension.
+        ellipse = self._get_ellipse(height, width)
+
+        # 2. Add color to the ellipse
+        color_list = np.array((0, 0, 0), dtype=np.uint8)
+        for c in range(channel):
+            color_list[c] = np.random.randint(1, 256) if self.use_rand_color else 255
+        face_arr[ellipse != 0] = color_list if channel == 3 else color_list[0]
+
+        # 3. Get occluded face and occlusion mask
+        msk = np.ones([height, width], dtype=np.uint8) * 255
+        msk[ellipse != 0] = 0
+
+        img_surpass = Image.fromarray(face_arr)
+        msk = Image.fromarray(msk)
+
+        return img_surpass, msk
+
+    def _get_ellipse(self, height, width):
+        """
+        Create random oval shape. \n
+        0: no occlusion \n
+        1~255: occluded by random gray value \n
+        """
+        ellipse = np.zeros([height, width], dtype=np.uint8)
+
+        ch = np.random.randint(height // 5, 4 * height // 5)
+        cw = np.random.randint(width // 5, 4 * width // 5)
+        ah = np.random.randint(20, min(ch, height - ch))
+        ratio = np.random.uniform(self.lo_ratio, self.hi_ratio)
+        aw = int(height * width * ratio / (3.14 * ah))
+        angle = 0
+        gray_val = np.random.randint(1, 256) if self.use_rand_color else 255
+
+        cv2.ellipse(ellipse, (cw, ch), (aw, ah), angle, 0, 360, gray_val, -1)
+
+        return ellipse
+
+
+""" Random Simply Connected Polygon Occlusion
+This transform can be used for training and testing.
+Init Params:
+    - connected_num: the num of simply connected graphs (default: 1)
+    - ratio: the area ratio of occlusion (default: 0.4)
+    - use_rand_color: use random color or not (default: True)
+    - lo_points_num: min number of points on a circle (default: 4) [lo, hi)
+    - hi_points_num: max number of points on a circle (default: 11) [lo, hi)
+    - use_circle: use circle or not (ellipse) (default: True)
+"""
+class RandomConnectedPolygon(object):
+    # TODO: Use triangles, refer to: https://blog.csdn.net/islittlehappy/article/details/81533090
+    def __init__(self,
+                 connected_num: int = 1,
+                 ratio: float = 0.4,
+                 use_rand_color: bool = True,
+                 lo_points_num: int = 4,
+                 hi_points_num: int = 11,
+                 use_circle: bool = True,
+                 ):
+        self.connected_num = connected_num
+        self.ratio = ratio
+        self.use_rand_color = use_rand_color
+        self.lo_points_num = lo_points_num
+        self.hi_points_num = hi_points_num
+        self.use_circle = use_circle
+
+    def __call__(self, img):
+        face_arr = np.array(img)
+        height, width = img.size[1], img.size[0]
+        channel = 1 if len(face_arr.shape) == 2 else 3
+
+        # 1. Generate the polygon shape (np.array), where 0 denotes no occlusion.
+        # The size of polygon shape (np.array) is (height, width) without channel dimension.
+        polygon = self._get_polygon(height, width)
+
+        # 2. Add color to the polygon
+        color_list = np.array((0, 0, 0), dtype=np.uint8)
+        for c in range(channel):
+            color_list[c] = np.random.randint(1, 256) if self.use_rand_color else 255
+        face_arr[polygon != 0] = color_list if channel == 3 else color_list[0]
+
+        # 3. Get occluded face and occlusion mask
+        msk = np.ones((height, width), dtype=np.uint8) * 255
+        msk[polygon != 0] = 0
+
+        img_surpass = Image.fromarray(face_arr)
+        msk = Image.fromarray(msk)
+
+        return img_surpass, msk
+
+    def _get_polygon(self, height, width):
+        """
+        Create random polygon (type: np.array).
+            - 0: no occlusion
+            - 1~255: occluded by random gray value
+        Process:
+            - Step1. Choose the center of a circle (or ellipse)
+            - Step2. Generate a big circle (or ellipse) and a small circle (or ellipse)
+            - Step3. Choose the first point on the big circle (or ellipse)
+            - Step4. Get next point on the big circle (or ellipse) or the small circle (or ellipse) step by step
+            - Step5. Paint the polygon according to these points with CV2
+        """
+        polygon = np.zeros((height, width), dtype=np.uint8)
+
+        # Choose the center
+        point_cnt = np.random.randint(self.lo_points_num, self.hi_points_num)
+        points = np.zeros((2 * point_cnt + 2, 2), dtype=np.int32)  # Should be np.int32!
+
+        center_x = np.random.randint(height // 5, 4 * height // 5)
+        center_y = np.random.randint(width // 5, 4 * width // 5)
+
+        # We need a big circle (or ellipse) and a small circle (or ellipse)
+        big_radius = np.random.randint(height // 5, 1.3 * height // 5)
+        small_radius = big_radius / np.random.uniform(1.3, 2.6)
+        big_angle, small_angle = 0, 0  # range: [0, 2pi]
+        get_next_point = self._calc_from_circle if self.use_circle else self._calc_from_ellipse
+
+        # First point
+        points[0] = get_next_point(big_radius, big_angle, center_x, center_y)
+
+        # Subsequent points
+        small_ind, big_ind = 0, 0
+        for big_ind in range(point_cnt):
+            big_angle += 2 * math.pi / point_cnt * np.random.uniform(0.7, 1.3)
+            points[big_ind + small_ind + 1] = get_next_point(big_radius, big_angle, center_x, center_y)
+
+            if np.random.random() > 0.5:
+                small_ind += 1
+                small_angle += 2 * math.pi / point_cnt * np.random.uniform(0.6, 1.4)
+                points[big_ind + small_ind + 1] = get_next_point(small_radius, small_angle, center_x, center_y)
+
+        # All points
+        points = points[:1 + big_ind + small_ind + 1]  # (first, (big...), (small...),)
+        points = np.array([points])  # Should be a 3d-array
+
+        gray_val = np.random.randint(1, 256) if self.use_rand_color else 255
+        cv2.fillPoly(polygon, points, gray_val)
+
+        return polygon
+
+    @staticmethod
+    def _calc_from_circle(radius, angle, center_x, center_y):
+        target_x = center_x + radius * math.cos(angle)
+        target_y = center_y + radius * math.sin(angle)
+        return np.array((int(target_x), int(target_y)))
+
+    @staticmethod
+    def _calc_from_ellipse(radius, angle, center_x, center_y):
+        radius_a = radius * np.random.uniform(0.5, 1.5)
+        radius_b = radius * np.random.uniform(0.5, 1.5)
+        target_x = center_x + radius_a * math.cos(angle)
+        target_y = center_y + radius_b * math.sin(angle)
+        return np.array((int(target_x), int(target_y)))
+
+
+
+
 if __name__ == '__main__':
 
     face = Image.open('Dan_Kellner_0001.jpg', 'r')
-    trans = RandomRectOcc(lo=0, hi=41,)
+    trans = RandomEllipse()
     for idx in range(100):
         ret, _ = trans(face)
         if idx < 15:
